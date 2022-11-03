f607ec8137244b101ea1009e2f0a987b4c3b70fd
SnzDev <tms.working@gmail.com>
2022-11-02 22:59:28 -0300

    feat: add cors dependency

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,13 @@ import express from "express";
 import { routes } from "./routes/routes";
 import "dotenv/config";
 import "express-async-errors";
-
+import cors from "cors";
 import { handleError } from "./Error";
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use(routes);
 
 app.use(handleError);
